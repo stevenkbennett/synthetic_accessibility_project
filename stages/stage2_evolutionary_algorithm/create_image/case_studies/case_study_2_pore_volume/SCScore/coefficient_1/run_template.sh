ecf4eeaa1b50702ddf4cf18c92790dadb4ab2ac0
stevenbennettonline <stevenbennettonline@gmail.com>
2019-10-23 19:34:35 +0100

    Modified some files

--- stages/stage2_evolutionary_algorithm/create_image/case_studies/case_study_2_pore_volume/SCScore/coefficient_1/run_template.sh
+++ stages/stage2_evolutionary_algorithm/create_image/case_studies/case_study_2_pore_volume/SCScore/coefficient_1/run_template.sh
@@ -3,7 +3,7 @@ ea_run_count=$(($(find run* -maxdepth 0 -type d | wc -l)+1))
 mkdir run$ea_run_count
 cp {input_template.pbs,input_template.py} run$ea_run_count
 cd run$ea_run_count
-mv input_template.pbs input.pbs
+mv input_template.pbs input$ea_run_count.pbs
 mv input_template.py input.py
 random_seed=$(shuf -i 1-65000 -n 1)
 sed -i "s/random_seed = 20/random_seed = $random_seed/g" input.py
--- stages/stage2_evolutionary_algorithm/create_image/case_studies/case_study_2_pore_volume/SCScore/coefficient_10/input_template.py
+++ stages/stage2_evolutionary_algorithm/create_image/case_studies/case_study_2_pore_volume/SCScore/coefficient_10/input_template.py
@@ -7,10 +7,12 @@ import logging
 from pathlib import Path
 import numpy as np
 import pywindow
+import sys
+from rdkit.Chem import AllChem as rdkit
 
 # Global settings.
 
-random_seed = 3
+random_seed = 2
 macromodel_path = '/rds/general/user/sb2518/home/opt/schrodinger2018-1'
 
 file_path = Path(__file__)
@@ -20,13 +22,17 @@ for parent in file_path.parents:
     if parent.name == 'create_image':
         base_image_path = parent
 
+sys.path.append(str(base_image_path))
+
+from utilities.scscore.scscore import SCScore # noqa
+
 logging.info('Loading input file.')
 
 # #####################################################################
 # Number of processes to start with the EA.
 # #####################################################################
 
-num_processes = 32
+num_processes = 24
 
 # #####################################################################
 # Set logging level.
@@ -60,18 +66,6 @@ debug_dumps = False
 
 tar_output = True
 
-
-# #####################################################################
-# Set attributes to write to JSON.
-# #####################################################################
-
-dump_attrs = [
-    'sa_score',
-    'largest_window',
-    'window_std',
-    'pore_diameter',
-]
-
 # #####################################################################
 # Initial population.
 # #####################################################################
@@ -83,6 +77,7 @@ building_blocks_path = base_image_path.joinpath(
     'all_precursors_filtered',
 )
 
+# Generator for all precursors.
 aldehydes = building_blocks_path.glob('**/aldehyde*.mol')
 amines = building_blocks_path.glob('**/amine*.mol')
 
@@ -127,6 +122,8 @@ population = stk.EAPopulation.init_diverse(
 generation_selector = stk.StochasticUniversalSampling(
     num_batches=population_size,
     random_seed=random_seed,
+    duplicate_batches=False,
+    duplicate_mols=False,
 )
 
 # #####################################################################
@@ -138,6 +135,7 @@ crossover_selector = stk.Tournament(
     num_batches=10,
     batch_size=2,
     duplicate_batches=False,
+    duplicate_mols=False,
     random_seed=random_seed,
 )
 
@@ -147,7 +145,7 @@ crossover_selector = stk.Tournament(
 
 mutation_selector = stk.Roulette(
     num_batches=5,
-    duplicate_mols=True,
+    duplicate_mols=False,
     batch_size=1,
     random_seed=random_seed,
 )
@@ -170,14 +168,14 @@ crosser = stk.GeneticRecombination(
 mutator = stk.Random(
     stk.RandomBuildingBlock(
         amine_building_blocks,
-        key=lambda mol: mol.func_groups[0].\
+        key=lambda mol: mol.func_groups[0].
         fg_type.name == 'primary_amine',
         random_seed=random_seed,
         use_cache=True,
     ),
     stk.SimilarBuildingBlock(
         amine_building_blocks,
-        key=lambda mol: mol.func_groups[0].\
+        key=lambda mol: mol.func_groups[0].
         fg_type.name == 'primary_amine',
         duplicate_building_blocks=False,
         random_seed=random_seed,
@@ -204,46 +202,68 @@ mutator = stk.Random(
 
 
 # Optimizer for full-run.
-optimizer = stk.Sequence(
-    stk.MacroModelForceField(
-        macromodel_path=macromodel_path,
-        restricted=True,
-        use_cache=True,
+failed_optimizer = stk.NullOptimizer(use_cache=True)
+
+optimizer = stk.TryCatch(
+    stk.Sequence(
+        stk.MacroModelForceField(
+            macromodel_path=macromodel_path,
+            restricted=True,
+            use_cache=True,
+        ),
+        stk.MacroModelForceField(
+            macromodel_path=macromodel_path,
+            restricted=False,
+            use_cache=True,
+        ),
+        stk.TryCatch(
+            stk.MacroModelMD(
+                macromodel_path=macromodel_path,
+                temperature=700,
+                eq_time=100,
+                use_cache=True,
+            ),
+            stk.NullOptimizer(
+                use_cache=True,
+            ),
+        ),
     ),
-    stk.MacroModelForceField(
-        macromodel_path=macromodel_path,
-        restricted=False,
-        use_cache=True,
-    ),
-    stk.MacroModelMD(
-        macromodel_path=macromodel_path,
-        temperature=700,
-        eq_time=100,
-        use_cache=True,
-    ),
+    failed_optimizer,
     use_cache=True,
 )
-
 
 # #####################################################################
 # Fitness Calculator.
 # #####################################################################
 
+
 def pore_diameter(mol):
     pw_mol = pywindow.Molecule.load_rdkit_mol(mol.to_rdkit_mol())
     mol.pore_diameter = pw_mol.calculate_pore_diameter()
     # Ideal pore diameter is 5 A.
-    return mol.pore_diameter
+    if (
+        mol.pore_diameter is not None or
+        isinstance(mol.pore_diameter, float)
+    ):
+        return abs(mol.pore_diameter-5.0)
+    else:
+        return mol.pore_diameter
 
 
 def largest_window(mol):
     pw_mol = pywindow.Molecule.load_rdkit_mol(mol.to_rdkit_mol())
     mol.largest_window = None
     windows = pw_mol.calculate_windows()
-    if windows is not None:
+    if windows is not None and len(windows) > 3:
         mol.largest_window = max(windows)
     # Ideal window diameter is 5 A.
-    return mol.largest_window
+    if (
+        mol.largest_window is not None or
+        isinstance(mol.largest_window, float)
+    ):
+        return abs(mol.largest_window-5.0)
+    else:
+        return mol.largest_window
 
 
 def window_std(mol):
@@ -255,16 +275,35 @@ def window_std(mol):
     return mol.window_std
 
 
+scscore = SCScore()
+
+
 def sa_score(mol):
-    return 10
+    scores = []
+    for bb in mol.get_building_blocks():
+        rdkit_mol = bb.to_rdkit_mol()
+        rdkit_mol.UpdatePropertyCache()
+        rdkit.GetSymmSSSR(rdkit_mol)
+        rdkit_mol.GetRingInfo()
+        scores.append(scscore.score(rdkit_mol))
+    mol.sa_score = sum(scores)
+    return mol.sa_score
 
 
-fitness_calculator = stk.PropertyVector(
+cage_fitness_calculator = stk.PropertyVector(
     pore_diameter,
     largest_window,
     window_std,
     sa_score,
 )
+
+fitness_calculator = stk.If(
+    condition=lambda mol: failed_optimizer.is_in_cache(mol),
+    true_calculator=stk.FitnessFunction(lambda mol: None),
+    false_calculator=cage_fitness_calculator,
+)
+
+
 
 # #####################################################################
 # Fitness normalizer.
@@ -278,14 +317,14 @@ def valid_fitness(population, mol):
 # Minimize synthetic accessibility and asymmetry.
 # Maximise pore volume and window size.
 fitness_normalizer = stk.Sequence(
-    stk.Power([1, 1, 1, 1], filter=valid_fitness),
+    stk.Power([-1, -1, -1, -1], filter=valid_fitness),
     stk.DivideByMean(filter=valid_fitness),
     # Coefficients of fitness function in order:
     # Pore volume: 10
     # Window size: 0
     # Asymmetry: 5
-    # Synthetic accessibility: 0
-    stk.Multiply([10, 0, 5, 0], filter=valid_fitness),
+    # Synthetic accessibility: 10
+    stk.Multiply([10, 0, 5, 10], filter=valid_fitness),
     stk.Sum(filter=valid_fitness),
     # Replace all fitness values that are lists with
     # minimum fitness / 2.
@@ -304,14 +343,22 @@ fitness_normalizer = stk.Sequence(
 # Exit condition.
 # #####################################################################
 
-terminator = stk.FitnessPlateau(
-    num_generations=5,
-    top_members=3,
-)
+terminator = stk.NumGenerations(50)
 
 # #####################################################################
 # Make plotters.
 # #####################################################################
+
+
+def apply(fn):
+
+    def filter_fn(mol):
+        return not hasattr(mol, fn.__name__)
+
+    def inner(progress):
+        all(True for _ in map(fn, filter(filter_fn, progress)))
+    return inner
+
 
 plotters = [
     stk.ProgressPlotter(
@@ -330,31 +377,35 @@ plotters = [
     ),
     stk.ProgressPlotter(
         filename='sascore_plot',
-        property_fn=lambda mol: mol.sa_score,
+        property_fn=lambda progress, mol: mol.sa_score,
         y_label='Synthetic Accessibility / unitless',
         filter=lambda progress, mol:
             mol.sa_score is not None,
+        progress_fn=apply(sa_score),
     ),
     stk.ProgressPlotter(
         filename='volume_plot',
-        property_fn=lambda mol: mol.pore_diameter,
+        property_fn=lambda progress, mol: mol.pore_diameter,
         y_label='Pore Diameter / A',
         filter=lambda progress, mol:
             mol.pore_diameter is not None,
+        progress_fn=apply(pore_diameter),
     ),
     stk.ProgressPlotter(
         filename='max_window_size',
-        property_fn=lambda mol: mol.largest_window,
+        property_fn=lambda progress, mol: mol.largest_window,
         y_label='Maximum Window Size / A',
         filter=lambda progress, mol:
             mol.largest_window is not None,
+        progress_fn=apply(largest_window),
     ),
     stk.ProgressPlotter(
         filename='window_std',
-        property_fn=lambda mol: mol.window_std,
+        property_fn=lambda progress, mol: mol.window_std,
         y_label='Std. Dev. of Window Diameters / A',
         filter=lambda progress, mol:
             mol.window_std is not None,
+        progress_fn=apply(window_std),
     )
 ]
 
