b433549099eb7c727c6a7ec83d8f7f4d2b017cba
Steven Bennett <stevenbennettonline@gmail.com>
2019-10-24 15:56:49 +0100

    Changed analyse_population

--- stages/stage3_ea_analysis/create_image/analyse_population.py
+++ stages/stage3_ea_analysis/create_image/analyse_population.py
@@ -1,7 +1,11 @@
-import stk
 from pathlib import Path
-from itertools import islice
 import re
+import importlib
+from stk import (
+    BuildingBlock,
+    ConstructedMolecule,
+    cage,
+)
 
 
 def load_population(pop_path):
@@ -9,8 +13,13 @@ def load_population(pop_path):
     with open(pop_path, 'r') as f:
         pop = []
         subpop = []
-        recording = False
+        if 'fitness' in pop_path:
+            recording = True
+        else:
+            recording = False
+
         gen = 1
+
         for line in f:
             # Initial population treated differently.
             if gen == 1:
@@ -37,20 +46,22 @@ def load_population(pop_path):
                     gen += 1
         return pop
 
-(\d)+ +\t(.*)\)\)\t\t\[((\d+.\d+), )+]\t(\d+.\d.)
-def load_stk_pop(pop):
-    '''
-    Loads stk populations.
-    '''
-    for gen in pop:
-        for mem in gen:
-            id = 
+
+def parse_population(pop):
+    # Import modules from stk.
+    stk_pop = []
+    for sp in pop:
+        subpop = []
+        for mem in sp:
+            p_bb = re.compile('(Const[^\t]+)')
+            stk_mem = eval(p_bb.search(mem)[0])
+            subpop.append(stk_mem)
+        stk_pop.append(stk_mem)
+    return stk_pop
 
 
-def main():
-    pop_path = Path('/Users/stevenbennett/Box/Steven/Work/PhD/Work/main_projects/synthetic_accessibility_project/stages/stage3_ea_analysis/create_image/example_pop.log')
+def get_stk_pop(pop_path):
+    pop_path = Path(pop_path)
     pop = load_population(str(pop_path))
-
-
-if __name__=='__main__':
-    main()
+    pop = parse_population(pop)
+    return pop
