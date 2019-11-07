dd24893fe64af15683803afeeb65ed2c3b231e18
stevenbennettonline <stevenbennettonline@gmail.com>
2019-11-07 11:38:13 +0000

    Updated tar

---
+++ stages/stage2_evolutionary_algorithm/create_image/case_studies/case_study_2_pore_volume/No_SA/tar_ea_run.py
@@ -0,0 +1,31 @@
+import tarfile
+from pathlib import Path
+import shutil
+import os
+
+current_dir = Path.cwd()
+ea_run = current_dir.joinpath('stk_ea_runs', '0')
+
+with tarfile.open(str(ea_run.joinpath('schrodinger_files.tar.gz')), "w:gz") as tar:
+    schrodinger_files = ea_run.joinpath('scratch')
+    prnit('Compressing Schrodinger files.')
+    for file in schrodinger_files.glob('[0-9]*'):
+        tar.add(file)
+        if file.is_dir():
+            shutil.rmtree(file)
+        else:
+            os.remove(file)
+    print('Finished compressing Schrodinger files.')
+
+with tarfile.open(str(ea_run.joinpath('ea_selection_files.tar.gz')), "w:gz") as tar:
+    schrodinger_files = ea_run.joinpath('scratch')
+    print('Compressing selection files.')
+    patts = ('crossover*', 'generational*', 'mutation*')
+    for patt in patts:
+        for file in schrodinger_files.glob(patt):
+            tar.add(file)
+            if file.is_dir():
+                shutil.rmtree(file)
+            else:
+                os.remove(file)
+    print('Finished compressing EA run graphs.')
